171090860e59b90b1e77fc57706c16d0b2b09201
alecs396 <a.swainston16@gmail.com>
2022-01-25 21:37:24 -0700

    Creating firestore database

---
+++ main.py
@@ -0,0 +1,9 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Database Setup
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
