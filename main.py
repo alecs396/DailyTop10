7322aaeabe6df2a0ce903d5abc7544467188375b
alecs396 <a.swainston16@gmail.com>
2022-01-26 00:12:23 -0700

    Created KV, Created classes in main.py

--- main.py
+++ main.py
@@ -3,17 +3,27 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
 
-# Database Setup
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
+class EntryLayout(Widget):
+        pass
 
-db = firestore.client()
+class MainApp(App):
+        def build(self):
+                
+                # Database Setup
+                cred = credentials.Certificate("serviceAccountKey.json")
+                firebase_admin.initialize_app(cred)
 
-"""
-# Test setup
-data = {"1) " : "Sam is Happy",
-        "2) " : "I have my degree"}
-db.collection("top 10").document("10-16-2022").set(data)
-"""
+                db = firestore.client()
+                return Builder.load_file('top10.kv')
+        
+        def submit(self):
+                pass
+        def show_records(self):
+                pass
+    
+if __name__ == '__main__':
+    MainApp().run()
