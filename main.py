7f478591b35eefafad9bc0af70195f621afe4cab
alecs396 <a.swainston16@gmail.com>
2022-01-26 01:03:22 -0700

    Submit button now loads premade entry to database

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
@@ -5,26 +6,25 @@ import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
+             
+# Database Setup
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
 
+db = firestore.client()
 
 class EntryLayout(Widget):
-        pass
+        def submit(self):
+                data = {"date" : "Today",
+                        "1" : "Sam is Happy"}
+                db.collection("top 10").document("1-26-2022").set(data)
 
 class MainApp(App):
         def build(self):
-                
-                # Database Setup
-                cred = credentials.Certificate("serviceAccountKey.json")
-                firebase_admin.initialize_app(cred)
-
-                db = firestore.client()
-                
                 # Load Builder
                 Builder.load_file('top10.kv')
                 return EntryLayout()
         
-        def submit(self):
-                pass
         def show_records(self):
                 pass
     
