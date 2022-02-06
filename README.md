e138c69e77419c097ab72af645c0c1ef4bbc53fa
alecs396 <a.swainston16@gmail.com>
2022-02-05 22:47:19 -0700

    Readme update. Config second screen. Prints to terminal

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # Overview
+## What is a Top 10 Journal?
+I keep a journal that I write in every morning.  The point of this journal is to list the top 10 things I want most in life. There are a few rules you need to follow in order for this journal to actually work and mean something to you:
 
+1. You write "I am happy and greatful because..."
+2. You then list up to 10 things that would make you happy. However you must write it in the past tense as if you have already accomplished or achieved it.
+3. You are not allowed to look at lists you have made in the past.  This makes it so that over time you truly start writing down things that you genuinely want in life.
 
+I have kept this journal for about six months and it is actually pretty incredible to look back at lists I have created and see how much of it has actually come true.
+
+## What this program does
+This program is a Top 10 Journal that creates entries and stores them in a FireStore database. It was created in Python and I used the Kivy library and language in order to create a GUI.
 
 [Software Demo Video](https://youtu.be/08pb3RzIPEE)
 
@@ -17,3 +26,8 @@
 * [Kivy Tutorial Playlist](https://www.youtube.com/playlist?list=PLCC34OHNcOtpz7PJQ7Tv7hqFBP_xDDjqg)
 * [Kivy Documentation PDF](https://buildmedia.readthedocs.org/media/pdf/kivy/latest/kivy.pdf)
 
+# Upcoming Changes
+* A second page for the user to select a view entries.
+* The ability to delete entries.
+* Entry boxes delete after submitting a new entry.
+* Change Color Scheme and Font Design
--- main.py
+++ main.py
@@ -13,11 +13,8 @@ cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
-
-
-class ViewerScreen(Screen):
-        pass
 class EntryLayout(Screen):
+        """This class is the Entry page.  It builds off of the top10.kv file."""
         date = ObjectProperty(None)
         r0 = ObjectProperty(None)
         r1 = ObjectProperty(None)
@@ -31,6 +28,7 @@ class EntryLayout(Screen):
         r9 = ObjectProperty(None)
         
         def submit(self):
+                """This function will submit an entry to the database."""
                 date = self.date.text
                 r0 = self.r0.text
                 r1 = self.r1.text
@@ -53,19 +51,56 @@ class EntryLayout(Screen):
                         "7" : r7,
                         "8" : r8,
                         "9" : r9}
+                # Send to database
                 db.collection("top 10").document(date).set(data)
 
+                # Create Message
+                self.ids.top_label.text = "Entry Submitted!"
+                
+                # Clear Entry Boxes
+                self.ids.date.text = ''
+                self.ids.r0.text = ''
+                self.ids.r1.text = ''
+                self.ids.r2.text = ''
+                self.ids.r3.text = ''
+                self.ids.r4.text = ''
+                self.ids.r5.text = ''
+                self.ids.r6.text = ''
+                self.ids.r7.text = ''
+                self.ids.r8.text = ''
+                self.ids.r9.text = ''
+class ViewerScreen(Screen):
+        date = ObjectProperty(None)
+        
+        def show_records(self):
+                date = self.date.text
+                
+                # Pull from database
+                records = db.collection("top 10").document(date).get()
+                 
+                if records.exists:
+                        result = records.to_dict()
+                        print(result)
+                        
+                        # Display Records
+                        # self.ids.record.text = text
+                else:
+                        print('DATE DOES NOT EXIST')
+                        # self.ids.record.text = "ENTRY DOES NOT EXIST"
+                
+                
+                       
+               
 class WindowManager(ScreenManager):
         pass
 
 class MainApp(App):
         def build(self):
                 # Load Builder
-                Builder.load_file('top10.kv')
-                return EntryLayout()
+                kv = Builder.load_file('top10.kv')
+                return kv
         
-        def show_records(self):
-                pass
+
     
 if __name__ == '__main__':
     MainApp().run()
